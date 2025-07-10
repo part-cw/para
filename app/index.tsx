959ddf43762bf720e23bedc834578f82daa9edba
munaibrahim680 <muna.ibr08@gmail.com>
2025-07-10 16:50:10 -0700

    fix: flash screen logic

--- app/index.tsx
+++ app/index.tsx
@@ -1,36 +1,15 @@
-// import { Text, View } from "react-native";
+import { Text, View } from "react-native";
 
-// export default function Index() {
-//   return (
-//     <View
-//       style={{
-//         flex: 1,
-//         justifyContent: "center",
-//         alignItems: "center",
-//       }}
-//     >
-//       <Text>Edit app/index.tsx to edit this screen.</Text>
-//     </View>
-//   );
-// }
-
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-
-SplashScreen.preventAutoHideAsync(); // Prevent auto-hide
-
-export default function Layout() {
-  useEffect(() => {
-    const hideSplash = async () => {
-      // delay for testing visuals -- TODO: remove
-      await new Promise(resolve => setTimeout(resolve, 10000));
-
-      await SplashScreen.hideAsync(); // Hide when ready
-    };
-
-    hideSplash();
-  }, []);
-
-  return <Stack />;
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
 }
