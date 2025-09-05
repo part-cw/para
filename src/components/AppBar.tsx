59275cde06ac202f1efb7fdf01555206a0010736
munaibrahim680 <muna.ibr08@gmail.com>
2025-09-05 12:54:03 -0700

    feat: AppBar - alert appears when home button pressed

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,6 +1,6 @@
 import { router, usePathname } from 'expo-router';
 import { useState } from 'react';
-import { Image, View } from 'react-native';
+import { Alert, Image, Platform, View } from 'react-native';
 import { Appbar, Button, Menu, Text, useTheme } from 'react-native-paper';
 import { ACTIVE_SITE, CURRENT_USER, DEVICE_ID_KEY } from '../config';
 
@@ -18,7 +18,6 @@ export default function AppBar() {
       mode='center-aligned'>
 
       {/* LEFT SIDE: Show SD logo if on Home Screen, else show 'Home' button */}
-      {/* TODO make sure Home button works on other screens */}
       <View style={{ flex: 1, alignItems: 'flex-start',  marginLeft: 12}}>
         {isHome ? (
           <Image source={require('../assets/images/SD-logo-text_side.png')}
@@ -26,20 +25,35 @@ export default function AppBar() {
                  width: 140}}
                  resizeMode="contain"/>)
           : (
-          <Button style={{ width: 110}}
-                  buttonColor={colors.secondary} 
-                  textColor={colors.onSecondary} 
-                  icon= 'home'
-                  mode="elevated" 
-                  onPress={() => {
-                    router.push('/')
-                    }}>
-              Home
-            </Button>)}
+          <Button 
+            style={{ width: 110}}
+            buttonColor={colors.secondary} 
+            textColor={colors.onSecondary} 
+            icon= 'home'
+            mode="elevated" 
+            onPress={() => {
+              // TODO - switch warning depending on which screen we're currently on. current message for data entry screens
+              if (Platform.OS === 'web') {
+                if (window.confirm("Are you sure you want to go home before submitting patient record? All data will be lost.")) {
+                  router.push('/')
+                }
+              } else {
+                Alert.alert(
+                  "Warning",
+                  "Are you sure you want to go home before submitting patient record? All data will be lost.",
+                  [
+                    { text: "Cancel", style: "cancel" },
+                    { text: "OK", onPress: () => router.push('/') }
+                  ]
+                );
+              }
+            }}
+          >
+            Home
+          </Button>)}
       </View>
 
       {/* RIGHT SIDE: User info with dropdown */}
-      {/* TODO make username + position update depending on who logged in */}
       <View style={{ flexDirection: 'column', flex: 1, alignItems: 'flex-end' }}>
         <Text style={{ color: colors.onPrimary, fontSize: 20, fontWeight: 'bold' }}>{CURRENT_USER}</Text> 
         <Text style={{ color: colors.onPrimary, fontSize: 16 }}>{ACTIVE_SITE} - Device {DEVICE_ID_KEY}</Text>
