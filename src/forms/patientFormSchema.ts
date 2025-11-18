7ca34a7850b28be1bfc0d9bff2e00dce55f9921f
munaibrahim680 <muna.ibr08@gmail.com>
2025-11-18 15:27:03 -0800

    fix: rename approxAge to approxAgeInYears

--- src/forms/patientFormSchema.ts
+++ src/forms/patientFormSchema.ts
@@ -29,7 +29,7 @@ export const patientFormSchema = [
         oneOf: [
             ['dob'],
             ['birthYear', 'birthMonth'],
-            ['approxAge']
+            ['approxAgeInYears']
         ],
         optionalFields: ['otherName']
     },
